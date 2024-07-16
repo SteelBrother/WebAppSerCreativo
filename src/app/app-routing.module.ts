824a276b063076c100378d341015c197070a3ff4
GioRide <giosketch23@gmail.com>
2024-07-15 19:43:12 -0500

    ajuste sliders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarrouselServiciosComponent } from './carrousel-servicios/carrousel-servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:  'Servicios', component: CarrouselServiciosComponent },
+{ path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
